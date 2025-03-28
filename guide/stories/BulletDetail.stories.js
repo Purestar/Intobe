416b973e5e218fe3916107da93d0fcc48dde4a3c
purestar <purestar@intobe.co.kr>
2025-03-28 14:33:34 +0900

    가이드: Bullet

--- guide/stories/BulletDetail.stories.js
+++ guide/stories/BulletDetail.stories.js
@@ -1,44 +1,23 @@
-import { bulletArgTypes, bulletArgs, generateMarkupStatic } from './commonStories';
+import { Bullet } from './Bullet';
+import { bulletArgTypes, bulletArgs } from './commonStories';
 
 export default {
-	title: 'UI/Bullet',
+	title: 'UI/Bullet/Detail',
 	render: (args) => Bullet(args),
 	tags: ['autodocs'],
 	argTypes: bulletArgTypes,
 	args: bulletArgs
 };
 
-const createMarkup = {
-	default: () => generateMarkupStatic(3, (i) => `<div>Item ${i}</div>`)
-}
+export const Circle = { args: { Type: 'bul-cir' }}
+export const Square = { args: { Type: 'bul-squ' }}
+export const Hyphen = { args: { Type: 'bul-hyp' }}
+export const Number = { args: { Type: 'bul-num' }}
+export const BulletColor = { args: { BulletColor: 'green' }}
+export const BulletGap = { args: { BulletGap: '20px' }}
+export const BulletSize = { args: { BulletSize: '20px' }}
+export const FontColor = { args: { FontColor: 'red' }}
+export const FontSize = { args: { FontSize: '20px' }}
+export const GapY = { args: { GapY: '20px' }}
+export const LineHeight = { args: { LineHeight: '20px' }}
 
-export const Circle = { args: { markup: `<div class="bul-cir">${createMarkup.default()}</div>` }}
-export const Square = { args: { markup: `<div class="bul-squ">${createMarkup.default()}</div>` }}
-export const Hyphen = { args: { markup: `<div class="bul-hyp">${createMarkup.default()}</div>` }}
-export const Number = { args: { markup: `<div class="bul-num">${createMarkup.default()}</div>` }}
-export const Color = { args: { markup: `<div class="bul-cir" style="--ft-c:red;">${createMarkup.default()}</div>` }}
-export const BulletColor = { args: { markup: `<div class="bul-cir" style="--bul-c:red;">${createMarkup.default()}</div>` }}
-export const Gap = { args: { markup: `<div class="bul-cir" style="--bul-gap:20px;">${createMarkup.default()}</div>` }}
-export const BulletSize = { args: { markup: `
-	<div class="layout-flex" style="--gap:20px;">
-		<div class="bul-cir" style="--bul-w:10px;">${createMarkup.default()}</div>
-		<div class="bul-squ" style="--bul-w:5px;">${createMarkup.default()}</div>
-		<div class="bul-hyp" style="--bul-w:7px; --bul-h:2px;">${createMarkup.default()}</div>
-	</div>
-` }}
-export const FontSize = { args: { markup: `
-	<div class="layout-flex" style="--gap:20px;">
-		<div class="bul-cir" style="--ft-s:10px;">${createMarkup.default()}</div>
-		<div class="bul-squ" style="--ft-s:15px;">${createMarkup.default()}</div>
-		<div class="bul-hyp" style="--ft-s:20px;">${createMarkup.default()}</div>
-		<div class="bul-num" style="--ft-s:25px;">${createMarkup.default()}</div>
-	</div>
-` }}
-export const LineHeight = { args: { markup: `
-	<div class="layout-flex" style="--gap:20px;">
-		<div class="bul-cir" style="--txt-lh:1;">${createMarkup.default()}</div>
-		<div class="bul-squ" style="--txt-lh:1.5;">${createMarkup.default()}</div>
-		<div class="bul-hyp" style="--txt-lh:2;">${createMarkup.default()}</div>
-		<div class="bul-num" style="--txt-lh:2.5;">${createMarkup.default()}</div>
-	</div>
-` }}
